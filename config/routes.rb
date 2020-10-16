60672b5f16e3f459d02dd0fd23fe0637979604e6
gie <macchaner@gmail.com>
2020-10-16 14:23:06 +0000

    corrected

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
     get 'signup', to: 'users#new'
     resources :users, only: [:index, :show, :new, :create]
     
-    resources :tasks, only: [:create, :destroy, ]
+    resources :tasks, only: [:create, :destroy, :index]
     
 end
